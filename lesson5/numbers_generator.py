3d62e9806e4ffd11efc635e49411019232749b95
IKishmar <irina.kishmar@gmail.com>
2023-05-18 11:50:28 +0300

    implementation tuple wihh numbers
    Signed-off-by: IKishmar <irina.kishmar@gmail.com>

--- lesson5/numbers_generator.py
+++ lesson5/numbers_generator.py
@@ -2,14 +2,12 @@ import random
 import os
 
 file_with_num = os.path.abspath('.')
-name = 'file.p'
-list_num = []
-my_list = []
+tuple_with_num = ()
 for num in range(0, 100):
     left_operand = str(random.randint(1, 100))
     right_operand = str(random.randint(1, 100))
     operator = str(random.randint(1, 3))
-    list_num = [left_operand, right_operand, operator]
-    my_str = ' '.join(list_num)
+    tuple_with_num = (left_operand, right_operand, operator)
+    my_str = ' '.join(tuple_with_num)
     with open('file_with_num', 'a') as file:
-        file.write(str(my_str) + '\n')
+        file.write(my_str + '\n')
