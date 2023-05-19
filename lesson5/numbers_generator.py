9bbcf330c82be4fadbd4457fd5f7797acc9b846e
IKishmar <irina.kishmar@gmail.com>
2023-05-19 10:06:26 +0300

    updating the tuple list implementation
    Signed-off-by: IKishmar <irina.kishmar@gmail.com>

--- lesson5/numbers_generator.py
+++ lesson5/numbers_generator.py
@@ -2,12 +2,16 @@ import random
 import os
 
 file_with_num = os.path.abspath('.')
-tuple_with_num = ()
+list_with_tuple = []
+tuple_with_num = tuple()
 for num in range(0, 100):
     left_operand = str(random.randint(1, 100))
     right_operand = str(random.randint(1, 100))
     operator = str(random.randint(1, 3))
     tuple_with_num = (left_operand, right_operand, operator)
-    my_str = ' '.join(tuple_with_num)
-    with open('file_with_num', 'a') as file:
-        file.write(my_str + '\n')
+    list_with_tuple.append(tuple_with_num)
+
+with open('file_with_num', 'a') as file:
+    for element in list_with_tuple:
+        element = ' '.join(element)
+        file.write(str(element) + '\n')
