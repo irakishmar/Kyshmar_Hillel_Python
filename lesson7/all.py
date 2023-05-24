f71148b28947c1d3e1e7430be9d27eeb6d37939b
IKishmar <irina.kishmar@gmail.com>
2023-05-24 21:44:49 +0300

    Implementation function all
    Signed-off-by: IKishmar <irina.kishmar@gmail.com>

---
+++ lesson7/all.py
@@ -0,0 +1,49 @@
+
+def check_fun(*args) -> bool:
+    false_list = [False, ' ', 0, [], {}, None]
+    result = True
+    for argument in args:
+        if argument in false_list:
+            result =  False
+    return result
+
+
+
+case_for_true1 =[True, True, True]
+case_for_true2 =[1, 2, 3, 4]
+case_for_true3 =['Bart', 'Liza', 'Homer']
+case_for_true4 =[1,2,3, ['Tom'], {1: 'Sara'}]
+case_for_true5 =[]
+
+case_for_false1 =[True, True, False]
+case_for_false2 =[1, 2, 3, 0]
+case_for_false3 =['Bart', [], 'Homer']
+case_for_false4 =[1,2,3, ['Tom'], {}]
+case_for_false5 =[None]
+
+if __name__ == "__main__":
+    result_for_true1 = (check_fun(*case_for_true1))
+    result_for_true2 = (check_fun(*case_for_true2))
+    result_for_true3 = (check_fun(*case_for_true3))
+    result_for_true4 = (check_fun(*case_for_true4))
+    result_for_true5 = (check_fun(*case_for_true5))
+
+    result_for_false1 = (check_fun(*case_for_false1))
+    result_for_false2 = (check_fun(*case_for_false2))
+    result_for_false3 = (check_fun(*case_for_false3))
+    result_for_false4 = (check_fun(*case_for_false4))
+    result_for_false5 = (check_fun(*case_for_false5))
+
+    print(f'For the {case_for_true1} case, the result is {result_for_true1}')
+    print(f'For the {case_for_true2} case, the result is {result_for_true2}')
+    print(f'For the {case_for_true3} case, the result is {result_for_true3}')
+    print(f'For the {case_for_true4} case, the result is {result_for_true4}')
+    print(f'For the {case_for_true5} case, the result is {result_for_true5}')
+
+    print(f'For the {case_for_false1} case, the result is {result_for_false1}')
+    print(f'For the {case_for_false2} case, the result is {result_for_false2}')
+    print(f'For the {case_for_false3} case, the result is {result_for_false3}')
+    print(f'For the {case_for_false4} case, the result is {result_for_false4}')
+    print(f'For the {case_for_false5} case, the result is {result_for_false5}')
+
+
