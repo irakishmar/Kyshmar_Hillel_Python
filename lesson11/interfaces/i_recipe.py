6ec6662d94cf9ed43142ec7867a07ed2f1cef3a9
IKishmar <irina.kishmar@gmail.com>
2023-06-16 15:32:01 +0300

    converting methods to protected
    Signed-off-by: IKishmar <irina.kishmar@gmail.com>

--- lesson11/interfaces/i_recipe.py
+++ lesson11/interfaces/i_recipe.py
@@ -3,13 +3,13 @@ from abc import ABC, abstractmethod
 
 class IRecipe(ABC):
     @abstractmethod
-    def take_ingredients(self, milk: int, meal: int, egg: int, strawberry: int):
+    def _take_ingredients(self, milk: int, meal: int, egg: int, strawberry: int):
         pass
 
     @abstractmethod
-    def mix_ingredients(self):
+    def _mix_ingredients(self):
         pass
 
     @abstractmethod
-    def move_to_baking_dish(self):
+    def _move_to_baking_dish(self):
         pass
