7d04507dde280b1c7a69df4ae1cc2ed2a23e2890
IKishmar <irina.kishmar@gmail.com>
2023-06-16 15:29:55 +0300

    converting methods to protected
    Signed-off-by: IKishmar <irina.kishmar@gmail.com>

--- lesson11/interfaces/i_cooking.py
+++ lesson11/interfaces/i_cooking.py
@@ -4,29 +4,29 @@ from abc import ABC, abstractmethod
 class ICooking(ABC):
 
     @abstractmethod
-    def open_oven(self):
+    def _open_oven(self):
         pass
 
     @abstractmethod
-    def put_in_the_oven(self):
+    def _put_in_the_oven(self):
         pass
 
     @abstractmethod
-    def close_oven(self):
+    def _close_oven(self):
         pass
 
     @abstractmethod
-    def waiting(self):
+    def _waiting(self):
         pass
 
     @abstractmethod
-    def turn_on_oven(self, oven_temperature: int):
+    def _turn_on_oven(self, oven_temperature: int):
         pass
 
     @abstractmethod
-    def take_out_from_the_oven(self):
+    def _take_out_from_the_oven(self):
         pass
 
     @abstractmethod
-    def turn_off_oven(self):
+    def _turn_off_oven(self):
         pass
